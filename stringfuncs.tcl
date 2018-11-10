524b31a1a9297e1d94e534886bb4f33baf698ee2
Karl Lehenbauer <karl@flightaware.com>
2018-11-10 04:03:44 +0000

    string functions for tcl expr, like strcat, strcmp, etc.

---
+++ stringfuncs.tcl
@@ -0,0 +1,13 @@
+
+
+proc tcl::mathfunc::strlen {string} {string length $string}
+
+proc tcl::mathfunc::strcat {args} {string cat {*}$args}
+
+proc tcl::mathfunc::srange {string first last} {string range $string $first $last}
+
+proc tcl::mathfunc::strcmp {string1 string2} {string compare $string1 $string2}
+
+proc tcl::mathfunc::strncmp {string1 string2 length} {string compare -length $length $string1 $string2}
+
+
