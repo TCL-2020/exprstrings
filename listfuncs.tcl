6ea1aad2106bccb433eeffabcfb46ad36cf07b4a
Karl Lehenbauer <karl@flightaware.com>
2018-11-10 04:54:40 +0000

    more string functions and some list functions

---
+++ listfuncs.tcl
@@ -0,0 +1,13 @@
+
+proc tcl::mathfunc::llength {list} {llength $list}
+
+proc tcl::mathfunc::lindex {list index} {lindex $list $index}
+
+proc tcl::mathfunc::lassign {list args} {lassign $list {*}$args}
+
+proc tcl::mathfunc::lappend {var args} {lappend $var {*}$args}
+
+proc tcl::mathfunc::lempty {list} {lempty $list}
+
+
+
--- stringfuncs.tcl
+++ stringfuncs.tcl
@@ -10,4 +10,26 @@ proc tcl::mathfunc::strcmp {string1 string2} {string compare $string1 $string2}
 
 proc tcl::mathfunc::strncmp {string1 string2 length} {string compare -length $length $string1 $string2}
 
+proc tcl::matchfunc::bytelength {string} {string bytelength $string}
+
+proc tcl::matchfunc::first {needleString haystackString} {string first $needleString $haystackString}
+
+proc tcl::matchfunc::last {needleString haystackString} {string last $needleString $haystackString}
+
+proc tcl::matchfunc::index {string index} {string index $string $index}
+
+proc tcl::matchfunc::tolower {string} {string tolower $string} 
+
+proc tcl::matchfunc::totitle {string} {string totitle $string} 
+
+proc tcl::matchfunc::toupper {string} {string toupper $string} 
+
+proc tcl::matchfunc::trim {string} {string trim $string} 
+proc tcl::matchfunc::trimleft {string} {string trimleft $string} 
+proc tcl::matchfunc::trimright {string} {string trimright $string} 
+
+proc tcl::matchfunc::repeat {string count} {string repeat $count} 
+
+#map, match, replace, reverse, wordend, or wordstart
+
 
